d8af5def7459d721d808e23bd5957d568c3ab397
Alexandr <belmymail@gmail.com>
2023-07-13 19:32:37 +0300

    Add files via upload

---
+++ HW_3/hw3_1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+free -h | awk {'print $6'} | sed 's/available/Свободно/g' | sed 's/Gi/Гб/g' | column -t 
+
+echo ' '
+mpstat | awk {'print $3,$4,$5,$6,$7,$8,$9,$10,$11,$12'} | sed 's/CPU/ЦП/g' | sed 's/usr/польз/g' | sed 's/sys/система/g' | column -t
+
+echo ' '
+ip -4 address | grep /24 | sed  's/inet/IP Адрес/g'
---
+++ HW_3/hw3_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo useradd -m $1
+
+sysinfo=/home/$1/sysinfo.info;
+
+#echo $userhomedir
+
+echo ====****==== >> $sysinfo
+lscpu >> $sysinfo
+
+echo ====****==== >> $sysinfo
+lsblk >> $sysinfo
+
+echo ====****==== >> $sysinfo
+uname >> $sysinfo
+
+echo ====****==== >> $sysinfo
+df >> $sysinfo
