bfc9e1dadf3c3cd1a4acab25a9cb74511601e6ab
NotMe <belmymail@gmail.com>
2023-08-23 14:25:44 +0300

    hw10 shfmt fix

--- HW_10/generate-testing-matrix.sh
+++ HW_10/generate-testing-matrix.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-os=("linux" "unix" "windows" "macos")
+os=('linux' 'unix' 'windows' 'macos')
 arch=('x86' 'amd64' 'mips' 'aarch64')
 
-for arch in "${arch[*]}"; do
-	for os in "${os[*]}"; do
-		echo $arch-$os
+for arch in "${arch[@]}"; do
+	for os in "${os[@]}"; do
+		echo "$arch"-"$os"
 	done
 done
