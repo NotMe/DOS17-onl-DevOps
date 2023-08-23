2edcb51775aa30bfd278b5f855e8921755afb91c
NotMe <belmymail@gmail.com>
2023-08-23 15:24:51 +0300

    hw10 shfmt fix

--- HW_10/not-empty-file.sh
+++ HW_10/not-empty-file.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-index=0
+while read -r file_name; do
+#	file_array["$index"]="$file_name"
 
-while read file_name; do
-	file_array[$index]="$file_name"
-	echo ${file_array[$index]}
-	index=$(($index + 1))
-done
-
-for file in "${file_array[*]}"; do
-	if [[ $(wc -m ${file_array[$file]}) -gt 0 ]]; then
-		echo "${file_array[$file]}"
+	if [[ $(stat -c %s "$file_name") -gt 0 ]]; then
+		echo "$file_name"
 	fi
 done
