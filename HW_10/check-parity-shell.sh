68243cf3eaf168de6162fc3a2f3a913296f4a650
NotMe <belmymail@gmail.com>
2023-08-23 13:14:32 +0300

    shfmt warnings fix

--- HW_10/check-parity-shell.sh
+++ HW_10/check-parity-shell.sh
@@ -2,9 +2,9 @@
 
 num=$1
 
-if [[ $num =~ ^[0-9]+$ ]] && [[ $(($num % 2)) -eq 0 ]]; then
+if [ $num =~ ^[0-9]+$ ] && [ $(($num % 2)) -eq 0 ]; then
 	echo $num "is EVEN"
-elif [[ $num =~ ^[0-9]+$ ]] && [[ $(($num % 2)) -ne 0 ]]; then
+elif [ $num =~ ^[0-9]+$ ] && [ $(($num % 2)) -ne 0 ]; then
 	echo $num "is ODD"
 else
 	echo $num "is NOT a NUMBER"
--- HW_10/generate-testing-matrix.sh
+++ HW_10/generate-testing-matrix.sh
@@ -3,8 +3,8 @@
 os=("linux" "unix" "windows" "macos")
 arch=('x86' 'amd64' 'mips' 'aarch64')
 
-for arch in ${arch[*]}; do
-	for os in ${os[*]}; do
+for arch in "${arch[*]}"; do
+	for os in "${os[*]}"; do
 		echo $arch-$os
 	done
 done
