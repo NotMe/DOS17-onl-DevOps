9a351a0d566c4415fb68e97ceb00f722ae6be9b1
Alexandr <belmymail@gmail.com>
2023-08-29 09:21:34 +0300

    fixed HW_14/encrypt.sh, HW_14/decrypt.sh

--- HW_14/decrypt.sh
+++ HW_14/decrypt.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-openssl rsautl -decrypt -inkey private.pem -in encrypt.dat -out new_encrypt.txt
-
-cat new_encrypt.txt
+#openssl rsautl -decrypt -inkey private.pem -in encrypt.dat -out new_encrypt.txt
+openssl rsautl -decrypt -inkey private.pem -in encrypt.dat
--- HW_14/encrypt.sh
+++ HW_14/encrypt.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-read -r "Enter a phrase: " phrase
-
-echo "${phrase}" > encrypt.txt
-
-openssl rsautl -encrypt -inkey public.pem -pubin -in encrypt.txt -out encrypt.dat
+openssl rsautl -encrypt -pubin -inkey public.pem
